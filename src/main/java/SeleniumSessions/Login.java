3189fa2bf5230eda3bbdd2192d7f63c900d42a2f
vchittamuru <vchittamuru@gmail.com>
2021-09-26 17:34:27 +0530

    login page test are added

---
+++ src/main/java/SeleniumSessions/Login.java
@@ -0,0 +1,13 @@
+package SeleniumSessions;
+
+import org.openqa.selenium.By;
+
+public class Login {
+
+	public static void main(String[] args) {
+
+		By username = By.id("txtusername");
+		By password = By.id("txtpwd");
+	}
+
+}
---
+++ src/test/java/LoginTest.java
@@ -0,0 +1,11 @@
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class LoginTest {
+
+	@Test
+	public void login() {
+		Assert.assertEquals(true, true);
+	}
+	
+}
